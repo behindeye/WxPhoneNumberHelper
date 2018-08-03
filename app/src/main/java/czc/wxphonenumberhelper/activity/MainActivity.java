f7d8df8e5579b36ffb3a09fb5a1175b89e02a30a
behindeye <15088132569@163.com>
2018-08-03 17:22:29 +0800

    加入toolbar

--- app/src/main/java/czc/wxphonenumberhelper/activity/MainActivity.java
+++ app/src/main/java/czc/wxphonenumberhelper/activity/MainActivity.java
@@ -7,9 +7,17 @@ import android.provider.Settings;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+import com.blankj.utilcode.util.AppUtils;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -38,210 +46,231 @@ import czc.wxphonenumberhelper.util.PreferenceHelper;
  */
 public class MainActivity extends BaseActivity {
 
-    private final static int MINI_VERSION_CODE = 16;
-    private final static String SNACKBAR_TXT = "当前您使用的系统版本过低，请更新高版本系统再使用";
+	private final static int MINI_VERSION_CODE = 16;
+	private final static String SNACKBAR_TXT = "当前您使用的系统版本过低，请更新高版本系统再使用";
 
-    @BindView(R.id.ll)
-    FrameLayout mLlRootView;
+	@BindView(R.id.ll)
+	LinearLayout mLlRootView;
 
-    @BindView(R.id.bannerContainer)
-    FrameLayout mFlBannerContainer;
+	@BindView(R.id.bannerContainer)
+	FrameLayout mFlBannerContainer;
 
-    @BindView(R.id.ryv)
-    RecyclerView mRecyclerView;
+	@BindView(R.id.ryv)
+	RecyclerView mRecyclerView;
 
-    private boolean mIsFirstJump = false;
-    private FuncAdapter mAdapter;
+	private boolean mIsFirstJump = false;
+	private FuncAdapter mAdapter;
 
-    @Override
-    protected void setContentView() {
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void setContentView() {
+		setContentView(R.layout.activity_main);
+	}
 
-    @Override
-    protected void initView() {
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mAdapter = new FuncAdapter(this, FuncItemFactory.getInstance().createFuncItems());
-        mRecyclerView.setAdapter(mAdapter);
-    }
+	@Override
+	protected void initView() {
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+//		toolbar.inflateMenu(R.menu.menu);
+		mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+		mAdapter = new FuncAdapter(this, FuncItemFactory.getInstance().createFuncItems());
+		mRecyclerView.setAdapter(mAdapter);
+	}
 
-    @Override
-    protected void initData() {
-        // only for build version larger equal than api 18
-        // systemVersionValicate();
-        checkUpdate();
-        saveSomeMsg();
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu,menu);
+		return super.onCreateOptionsMenu(menu);
+	}
 
-    @Override
-    protected void setListener() {
-        mAdapter.setOnItemClickListener(new FuncAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(int position) {
-                String funcName = FuncItemFactory.getInstance().createFuncItems().get(position).funcName;
-                switch (funcName) {
-                    case FuncItemFactory.CREAT_ENUMBER:
-                        redirect(NumberActivity.class);
-                        break;
-                    case FuncItemFactory.SHARE_GROUP:
-                        redirect(GroupActivity.class);
-                        break;
-                    case FuncItemFactory.QUICK_CONTACTPEOPLE:
-                        startAddPeople(Const.KEY_QUICK_ADD_NEAR_PEOPLE);
-                        break;
-                    case FuncItemFactory.ADD_CONTACTPEOPLE:
-                        startAddPeople(Const.KEY_ADD_FRIEND);
-                        break;
-                    case FuncItemFactory.ADD_NEARPEOPLE:
-                        startAddPeople(Const.KEY_ADD_NEAR_PEOPLE);
-                        break;
-                    case FuncItemFactory.STOP_ADD_PEOPLE:
-                        PreferenceHelper.putBoolean(Const.PREF_KEY_STOP_AUTO_ADD_PEOPLE_FLAG, true);
-                        EventBus.getDefault().post(new Boolean(true));
-                        toast("设置成功");
-                        break;
-                    case FuncItemFactory.FLOW_WINDOW:
-                        checkPermissonOrOpenWindow();
-                        break;
-                    case FuncItemFactory.VALIDATE:
-                        redirect(SentenceActivity.class);
-                        break;
-                    case FuncItemFactory.FEEDBACK:
-                        redirect(FeedbackActivity.class);
-                        break;
-                    case FuncItemFactory.SOFT_VALIDATE:
-                        redirect(ValidateActivity.class);
-                        break;
-                    case FuncItemFactory.HELP:
-                        redirect(HelpActivity.class);
-                        break;
-                }
-            }
-        });
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.action_feedback:
+				redirect(FeedbackActivity.class);
+				break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    private void checkPermissonOrOpenWindow() {
-        if (FloatWindowUtil.getInstance().checkPermission(getApplicationContext())) {
-      		startService(new Intent(this, WxHelperService.class));
-        }else {
-            FloatWindowUtil.getInstance().applyPermission(getApplicationContext());
-        }
-    }
+	@Override
+	protected void initData() {
+		// only for build version larger equal than api 18
+		// systemVersionValicate();
+		checkUpdate();
+//		saveSomeMsg();
+	}
 
+	@Override
+	protected void setListener() {
+		mAdapter.setOnItemClickListener(new FuncAdapter.OnItemClickListener() {
+			@Override
+			public void onItemClick(int position) {
+				String funcName = FuncItemFactory.getInstance().createFuncItems().get(position).funcName;
+				switch (funcName) {
+					case FuncItemFactory.CREAT_ENUMBER:
+						redirect(NumberActivity.class);
+						break;
+					case FuncItemFactory.SHARE_GROUP:
+						redirect(GroupActivity.class);
+						break;
+					case FuncItemFactory.QUICK_CONTACTPEOPLE:
+						startAddPeople(Const.KEY_QUICK_ADD_NEAR_PEOPLE);
+						break;
+					case FuncItemFactory.ADD_CONTACTPEOPLE:
+						startAddPeople(Const.KEY_ADD_FRIEND);
+						break;
+					case FuncItemFactory.ADD_NEARPEOPLE:
+						startAddPeople(Const.KEY_ADD_NEAR_PEOPLE);
+						break;
+					case FuncItemFactory.STOP_ADD_PEOPLE:
+						PreferenceHelper.putBoolean(Const.PREF_KEY_STOP_AUTO_ADD_PEOPLE_FLAG, true);
+						EventBus.getDefault().post(new Boolean(true));
+						toast("设置成功");
+						break;
+					case FuncItemFactory.FLOW_WINDOW:
+						checkPermissonOrOpenWindow();
+						break;
+					case FuncItemFactory.VALIDATE:
+						redirect(SentenceActivity.class);
+						break;
+					case FuncItemFactory.FEEDBACK:
+						redirect(FeedbackActivity.class);
+						break;
+					case FuncItemFactory.SOFT_VALIDATE:
+						redirect(ValidateActivity.class);
+						break;
+					case FuncItemFactory.HELP:
+						redirect(HelpActivity.class);
+						break;
+				}
+			}
+		});
+	}
 
-    private void startAddPeople(final int addType) {
-        if (PreferenceHelper.getBoolean(Const.PREF_IS_FIRST_USE_ADD_FUNCTION, true)) {
-            DialogView dv = new DialogView(this);
-            dv.setTitle("测试功能");
-            dv.setMessage(getString(R.string.auto_add_people_tips));
-            dv.setCancel(null);
-            dv.AddDialogClickListener(new OnDialogClickListener() {
-                @Override
-                public void OnOKClickListener(View v) {
-                    PreferenceHelper.putBoolean(Const.PREF_IS_FIRST_USE_ADD_FUNCTION, false);
-                    prepareAddPeople(addType);
-                }
-
-                @Override
-                public void OnCancelClickListener(View v) {
-                }
-
-                @Override
-                public void OnCustomClickListener(View v) {
-                }
-            });
-            dv.show();
-        } else {
-            prepareAddPeople(addType);
-        }
-    }
-
-    private void prepareAddPeople(int addType) {
-        PreferenceHelper.putBoolean(Const.PREF_KEY_STOP_AUTO_ADD_PEOPLE_FLAG, false);
-        PreferenceHelper.putInt(Const.PREF_KEY_ADD_PEOPLE_TYPE, addType);
-        EventBus.getDefault().post(new Integer(addType));
-        if (!WxAppUtil.checkPermission()) {
-            jumpSetting();
-            mIsFirstJump = true;
-        } else {
-            WxAppUtil.jumpToWx(this);
-        }
-    }
+	private void checkPermissonOrOpenWindow() {
+		if (FloatWindowUtil.getInstance().checkPermission(getApplicationContext())) {
+			startService(new Intent(this, WxHelperService.class));
+		} else {
+			FloatWindowUtil.getInstance().applyPermission(getApplicationContext());
+		}
+	}
 
 
-    private void systemVersionValicate() {
-        if (Build.VERSION.SDK_INT < MINI_VERSION_CODE) {
+	private void startAddPeople(final int addType) {
+		if (PreferenceHelper.getBoolean(Const.PREF_IS_FIRST_USE_ADD_FUNCTION, true)) {
+			DialogView dv = new DialogView(this);
+			dv.setTitle("测试功能");
+			dv.setMessage(getString(R.string.auto_add_people_tips));
+			dv.setCancel(null);
+			dv.AddDialogClickListener(new OnDialogClickListener() {
+				@Override
+				public void OnOKClickListener(View v) {
+					PreferenceHelper.putBoolean(Const.PREF_IS_FIRST_USE_ADD_FUNCTION, false);
+					prepareAddPeople(addType);
+				}
 
-            Snackbar.make(mLlRootView, SNACKBAR_TXT, Snackbar.LENGTH_LONG).show();
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    MainActivity.this.finish();
-                }
-            }, 3500);
-        }
-    }
+				@Override
+				public void OnCancelClickListener(View v) {
+				}
+
+				@Override
+				public void OnCustomClickListener(View v) {
+				}
+			});
+			dv.show();
+		} else {
+			prepareAddPeople(addType);
+		}
+	}
+
+	private void prepareAddPeople(int addType) {
+		PreferenceHelper.putBoolean(Const.PREF_KEY_STOP_AUTO_ADD_PEOPLE_FLAG, false);
+		PreferenceHelper.putInt(Const.PREF_KEY_ADD_PEOPLE_TYPE, addType);
+		EventBus.getDefault().post(new Integer(addType));
+		if (!WxAppUtil.checkPermission()) {
+			jumpSetting();
+			mIsFirstJump = true;
+		} else {
+			WxAppUtil.jumpToWx(this);
+		}
+	}
 
 
-    private void jumpSetting() {
-        // jump to setting permission
-        Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
-        startActivity(intent);
-    }
+	private void systemVersionValicate() {
+		if (Build.VERSION.SDK_INT < MINI_VERSION_CODE) {
+
+			Snackbar.make(mLlRootView, SNACKBAR_TXT, Snackbar.LENGTH_LONG).show();
+			new Handler().postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					MainActivity.this.finish();
+				}
+			}, 3500);
+		}
+	}
 
 
-    @Override
-    protected void onResume() {
-        if (mIsFirstJump && WxAppUtil.checkPermission()) {
-            WxAppUtil.jumpToWx(this);
-            mIsFirstJump = false;
-        }
-        super.onResume();
-    }
+	private void jumpSetting() {
+		// jump to setting permission
+		Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+		startActivity(intent);
+	}
 
-    private void checkUpdate() {
-        BmobUpdateAgent.update(this);
-        if (Const.DEBUG) {
-            BmobUpdateAgent.setUpdateListener(new BmobUpdateListener() {
-                @Override
-                public void onUpdateReturned(int updateStatus, UpdateResponse updateResponse) {
-                    log(updateStatus + "");
-                    if (updateStatus == UpdateStatus.Yes) {
-                        toast("版本更新");
-                    } else if (updateStatus == UpdateStatus.No) {
-                        toast("版本无更新");
-                    } else if (updateStatus == UpdateStatus.EmptyField) {
-                        toast("1、target_size（文件大小）是否填写；2、path或者android_url两者必填其中一项。");
-                    } else if (updateStatus == UpdateStatus.IGNORED) {
-                        toast("该版本已被忽略更新");
-                    } else if (updateStatus == UpdateStatus.ErrorSizeFormat) {
-                        toast("请使用file.length()方法获取apk大小。");
-                    } else if (updateStatus == UpdateStatus.TimeOut) {
-                        toast("超时");
-                    }
-                }
-            });
-        }
-    }
 
-    private void saveSomeMsg() {
-        if (WxAppUtil.isNewUser() && WxAppUtil.isValiditySoftWare()) {
-            String manufacturer = android.os.Build.MANUFACTURER == null ? "" : android.os.Build.MANUFACTURER;
-            String model = android.os.Build.MODEL == null ? "" : android.os.Build.MODEL;
-            UserManager userManager = new UserManager();
-            userManager.userName = PreferenceHelper.getString(Const.PREF_KEY_ACCOUNT, "");
-            userManager.imei = ((TelephonyManager) getSystemService(TELEPHONY_SERVICE)).getDeviceId();
-            userManager.manufacturer = manufacturer + model;
-            userManager.save(new SaveListener<String>() {
-                @Override
-                public void done(String s, BmobException e) {
-                    if (e == null) {
-                        PreferenceHelper.putBoolean(Const.PREF_IS_NEW_USER, false);
-                    }
-                }
-            });
-        }
-    }
+	@Override
+	protected void onResume() {
+		if (mIsFirstJump && WxAppUtil.checkPermission()) {
+			WxAppUtil.jumpToWx(this);
+			mIsFirstJump = false;
+		}
+		super.onResume();
+	}
+
+	private void checkUpdate() {
+		BmobUpdateAgent.update(this);
+		if (Const.DEBUG) {
+			BmobUpdateAgent.setUpdateListener(new BmobUpdateListener() {
+				@Override
+				public void onUpdateReturned(int updateStatus, UpdateResponse updateResponse) {
+					log(updateStatus + "");
+					if (updateStatus == UpdateStatus.Yes) {
+						toast("版本更新");
+					} else if (updateStatus == UpdateStatus.No) {
+						toast("版本无更新");
+					} else if (updateStatus == UpdateStatus.EmptyField) {
+						toast("1、target_size（文件大小）是否填写；2、path或者android_url两者必填其中一项。");
+					} else if (updateStatus == UpdateStatus.IGNORED) {
+						toast("该版本已被忽略更新");
+					} else if (updateStatus == UpdateStatus.ErrorSizeFormat) {
+						toast("请使用file.length()方法获取apk大小。");
+					} else if (updateStatus == UpdateStatus.TimeOut) {
+						toast("超时");
+					}
+				}
+			});
+		}
+	}
+
+	private void saveSomeMsg() {
+		if (WxAppUtil.isNewUser() && WxAppUtil.isValiditySoftWare()) {
+			String manufacturer = android.os.Build.MANUFACTURER == null ? "" : android.os.Build.MANUFACTURER;
+			String model = android.os.Build.MODEL == null ? "" : android.os.Build.MODEL;
+			UserManager userManager = new UserManager();
+			userManager.userName = PreferenceHelper.getString(Const.PREF_KEY_ACCOUNT, "");
+			userManager.imei = ((TelephonyManager) getSystemService(TELEPHONY_SERVICE)).getDeviceId();
+			userManager.manufacturer = manufacturer + model;
+			userManager.save(new SaveListener<String>() {
+				@Override
+				public void done(String s, BmobException e) {
+					if (e == null) {
+						PreferenceHelper.putBoolean(Const.PREF_IS_NEW_USER, false);
+					}
+				}
+			});
+		}
+	}
 
 
 //    public boolean onKeyDown(int keyCode, KeyEvent event) {
--- app/src/main/java/czc/wxphonenumberhelper/factory/FuncItemFactory.java
+++ app/src/main/java/czc/wxphonenumberhelper/factory/FuncItemFactory.java
@@ -19,14 +19,16 @@ public class FuncItemFactory {
 
     private static FuncItemFactory instance;
     public static final String CREAT_ENUMBER = "生成手机好友";
+    @Deprecated
     public static final String SHARE_GROUP = "群组分享";
-    public static final String QUICK_CONTACTPEOPLE = "快速加手机好友";
+    public static final String QUICK_CONTACTPEOPLE = "加手机好友";
     public static final String ADD_CONTACTPEOPLE = "自动加手机好友";
-    public static final String ADD_NEARPEOPLE = "自动向附近的人打招呼";
+    public static final String ADD_NEARPEOPLE = "向附近的人打招呼";
     public static final String STOP_ADD_PEOPLE = "停止加好友";
     public static final String FLOW_WINDOW = "显示悬浮窗";
     public static final String VALIDATE = "加人验证信息";
     public static final String FEEDBACK = "意见反馈";
+	@Deprecated
     public static final String SOFT_VALIDATE = "软件激活";
     public static final String HELP = "帮助";
 
@@ -54,8 +56,8 @@ public class FuncItemFactory {
                 add(new FuncItem(FLOW_WINDOW, "显示悬浮窗", R.drawable.icon_flow_window, false));
                 add(new FuncItem(VALIDATE, "添加好友时输入的验证信息", R.drawable.icon_validate, false));
 //                add(new FuncItem(SOFT_VALIDATE, "激活软件使用", R.drawable.icon_validity, false));
-                add(new FuncItem(SHARE_GROUP, "发布群组信息，认识好友", R.drawable.icon_group, false));
-                add(new FuncItem(FEEDBACK, "有问题，及时反馈给我们", R.drawable.icon_feedback, false));
+//                add(new FuncItem(SHARE_GROUP, "发布群组信息，认识好友", R.drawable.icon_group, false));
+//                add(new FuncItem(FEEDBACK, "有问题，及时反馈给我们", R.drawable.icon_feedback, false));
                 add(new FuncItem(HELP, "常见问题", R.drawable.icon_help, false));
             }
         };
